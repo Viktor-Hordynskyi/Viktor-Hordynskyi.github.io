0e19631a40743dbf24742df1739e62628747ef3c
@Morpi <work.morpi@gmail.com>
2018-07-02 16:13:20 +0300

    edit

--- js/main.js
+++ js/main.js
@@ -21,68 +21,9 @@ function drawing() {
 		ctx.fillRect(randomX, randomY, randomSize, randomSize);
 	}
 }
-drawing();
-// var c = document.getElementById('star-sky');
-// var ctx = c.getContext('2d');
-// var x = c.width = window.innerWidth;
-// var y = c.height = window.innerHeight;
-// var nStar = Math.round((x + y)/10);
-// var randomSize = Math.floor((Math.random()*2)+1);
-
-// var stars = [];
-
-// var fps = 0;
-// setInterval(function() {
-//   console.log(fps);
-//   fps = 0;
-// }, 1000);
-// function createStars() {
-// 	'use strict';
-// 	stars = [];
-// 	for(var i=0; i<=nStar; i++) {
-// 		stars.push({
-// 			x: Math.random() * x,
-// 			y: Math.random() * y,
-// 			o: Math.random(),
-// 			r: Math.random(),
-// 			s: 0.02,
-// 		})
-// 		if(randomSize>.1) {
-// 			ctx.shadowBlur = Math.floor((Math.random()*15));
-// 			ctx.shadowColor = "white";
-// 		}
-// 	}
-// }
-
-// function drawing() {
-// 	fps++;
-// 	'use strict';
-// 	requestAnimationFrame(drawing);
-// 	ctx.clearRect(0, 0, innerWidth, innerHeight);
-// 	for(var i=0; i<=nStar; i++) {
-// 		var e = stars[i];
-// 		if (e.o > 1 || e.o < 0) {
-// 			e.s = -e.s;
-// 		} 
-// 		e.o += e.s;
-// 		ctx.beginPath();
-// 		ctx.arc(e.x, e.y, e.r, 0, Math.PI*2, false);
-// 		ctx.strokeStyle = 'rgba(255, 255, 255, '+e.o+')';
-// 		ctx.stroke();
-// 	}
-// }
-// window.onload = function () {
-// 	createStars();
-// 	drawing();
-// };
-// window.onresize = function () {
-// 	var x = c.width = window.innerWidth;
-// 	var y = c.height = window.innerHeight;
-// 	createStars();
-// 	drawing();
-// };
-
-
+$(window).on('load resize', function() {
+	drawing();	
+})
 
 $(window).on('load scroll', function() {
 	// show navbar when scroll page
--- js/test.js
+++ js/test.js
@@ -46,73 +46,60 @@ drawing();
 
 // var c = document.getElementById('star-sky');
 // var ctx = c.getContext('2d');
-// var xMax = c.width = window.screen.availWidth;
-// var yMax = c.height = window.screen.availHeight;
-// var hmTimes = Math.round(xMax + yMax);
+// var x = c.width = window.innerWidth;
+// var y = c.height = window.innerHeight;
+// var nStar = Math.round((x + y)/10);
 // var randomSize = Math.floor((Math.random()*2)+1);
-// var randomHue = Math.floor((Math.random()*360)+1);
-
 
 // var stars = [];
-// function creatStars() {
+
+// var fps = 0;
+// setInterval(function() {
+//   console.log(fps);
+//   fps = 0;
+// }, 1000);
+// function createStars() {
+// 	'use strict';
 // 	stars = [];
-// 	for(var i=0; i<=1; i++) {
-// 		// var randomX = Math.floor((Math.random()*xMax)+1);
-// 		// var randomY = Math.floor((Math.random()*yMax)+1);
-// 		// var randomOpacityOne = Math.floor((Math.random()*9)+1);
-// 		// var randomOpacityTwo = Math.floor((Math.random()*9)+1);
+// 	for(var i=0; i<=nStar; i++) {
 // 		stars.push({
-// 			x: Math.floor((Math.random()*xMax)+1),
-// 			y: Math.floor((Math.random()*yMax)+1),
-// 			o1: Math.floor((Math.random()*9)+1),
-// 			o2: Math.floor((Math.random()*9)+1),
+// 			x: Math.random() * x,
+// 			y: Math.random() * y,
+// 			o: Math.random(),
+// 			r: Math.random(),
+// 			s: 0.02,
 // 		})
-// 		if(randomSize>1) {
-// 			ctx.shadowBlur = Math.floor((Math.random()*15)+5);
+// 		if(randomSize>.1) {
+// 			ctx.shadowBlur = Math.floor((Math.random()*15));
 // 			ctx.shadowColor = "white";
 // 		}
 // 	}
 // }
 
 // function drawing() {
-// 	for(var i=0; i<=1; i++) {
+// 	fps++;
+// 	'use strict';
+// 	requestAnimationFrame(drawing);
+// 	ctx.clearRect(0, 0, innerWidth, innerHeight);
+// 	for(var i=0; i<=nStar; i++) {
 // 		var e = stars[i];
-// 		// console.log('drawing o1-'+e.o1);
-// 		// console.log('drawing o2-'+e.o2);
-// 		ctx.fillStyle = "hsla("+randomHue+", 30%, 80%, ."+e.o1+e.o2+")";
-// 		ctx.fillRect(e.x, e.y, randomSize, randomSize);
-
-// 		// ctx.fill();
+// 		if (e.o > 1 || e.o < 0) {
+// 			e.s = -e.s;
+// 		} 
+// 		e.o += e.s;
+// 		ctx.beginPath();
+// 		ctx.arc(e.x, e.y, e.r, 0, Math.PI*2, false);
+// 		ctx.strokeStyle = 'rgba(255, 255, 255, '+e.o+')';
+// 		ctx.stroke();
 // 	}
-// 		// console.log(ctx);
-// 		// requestAnimationFrame(drawing);
-// 		// ctx.fill();
-// 	}
-// 	// setInterval(drawing, 1000);
-// 	// console.log(randomOpacityOne);
-// 	// console.log(randomOpacityTwo);
-
-// 	function update() {
-// 		for(var i=0; i<=1; i++) {
-// 			var e = stars[i];
-// 			e.o1 = Math.floor((Math.random()*9)+1);
-// 			e.o2 = Math.floor((Math.random()*9)+1);
-// 			// console.log('update o1-'+e.o1);
-// 			// console.log('update o2-'+e.o2);
-// 		}
-// 	// console.log(randomOpacityOne);
-// 	// console.log(randomOpacityTwo);
 // }
-// creatStars();
-// console.log(stars);
-// // creatStars();
-// // drawing();
-// // update();
-// function pulse() {
-	
+// window.onload = function () {
+// 	createStars();
 // 	drawing();
-
-// 	update();
-// 	// setInterval(pulse, 1000);
-// }
-// pulse();
+// };
+// window.onresize = function () {
+// 	var x = c.width = window.innerWidth;
+// 	var y = c.height = window.innerHeight;
+// 	createStars();
+// 	drawing();
+// };
