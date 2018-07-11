300d501c126fe47b05ed0abcba20779a2ed6ff6c
@Morpi <work.morpi@gmail.com>
2018-07-11 12:17:51 +0300

    edit

--- projectGT/js/main.js
+++ projectGT/js/main.js
@@ -1,6 +1,5 @@
 $(window).on('resize load', function() {
 	if(window.matchMedia('(min-width: 1200px)').matches) {
-		console.log(window.matchMedia('(min-width: 1200px)').matches);
 		$('#gt').children().children().each(function() {
 			$(this).hover(function() {
 				$(this).find('.tooltiptext').addClass('active');
@@ -9,7 +8,6 @@ $(window).on('resize load', function() {
 			});
 		});
 	} else {
-		console.log(window.matchMedia('(min-width: 1200px)').matches);
 		$('#gt').find('.tooltip-mobile').each(function() {
 			$(this).hover(function() {
 				$(this).siblings('.tooltiptext').addClass('active');
