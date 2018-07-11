3cabf4873bd15e32d09dd1eba69c185e43175523
@Morpi <work.morpi@gmail.com>
2018-07-11 12:13:18 +0300

    edit

--- projectGT/js/main.js
+++ projectGT/js/main.js
@@ -1,9 +1,21 @@
-$(document).ready(function() {
-	$('#gt').children().children().each(function() {
-		$(this).hover(function() {
-			$(this).find('.tooltiptext').addClass('active');
-		}, function() {
-			$('.tooltiptext').removeClass('active');
+$(window).on('resize load', function() {
+	if(window.matchMedia('(min-width: 1200px)').matches) {
+		console.log(window.matchMedia('(min-width: 1200px)').matches);
+		$('#gt').children().children().each(function() {
+			$(this).hover(function() {
+				$(this).find('.tooltiptext').addClass('active');
+			}, function() {
+				$('.tooltiptext').removeClass('active');
+			});
 		});
-	});
+	} else {
+		console.log(window.matchMedia('(min-width: 1200px)').matches);
+		$('#gt').find('.tooltip-mobile').each(function() {
+			$(this).hover(function() {
+				$(this).siblings('.tooltiptext').addClass('active');
+			}, function() {
+				$('.tooltiptext').removeClass('active');
+			});
+		});
+	}	
 });
