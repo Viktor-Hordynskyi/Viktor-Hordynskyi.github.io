04992c1d05b106d1664c7ca2bdefb3f037158ab6
Viktor <work.morpi@gmail.com>
2018-09-29 21:31:36 +0300

    edit

---
+++ projectVickyPid/js/main.js
@@ -0,0 +1,8 @@
+// animate links - anchor(#)
+  $('a').on('click', function(){
+  	var anchor = $(this);
+  	$('html, body').stop().animate({
+  		scrollTop: $(anchor.attr('href')).offset().top
+  	}, 1500, 'linear')
+  	event.preventDefault();
+  })
