9370f218c41ae84a7f591a2781dfe10628fd1375
Viktor <work.morpi@gmail.com>
2018-10-04 23:09:18 +0300

    edit

--- projectFlorin/js/main.js
+++ projectFlorin/js/main.js
@@ -9,22 +9,3 @@ $(document).ready(function() {
 		event.preventDefault();
 	})
 })
-
-// Add indent 88px for scrolling anchor(#)
-	// $(document).ready(onScroll);
-	// $(document).on("scroll", onScroll);
-	// $("a").click(function(e) {
-	// 	$(document).off('scroll');
-	// 	$('#header a.active').removeClass('active');
-	// 	$(this).addClass('active');
-
-	// 	var hash = $(this).attr('href'),
-	// 	target = $(hash),
-	// 	hh = $('header').innerHeight();
-
-	// 	$('html, body').stop().animate({
-	// 		'scrollTop': target.offset().top - hh
-	// 	}, 1500, function () {
-	// 		$(document).on('scroll', onScroll);
-	// 	});
-	// });
