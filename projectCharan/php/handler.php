d21fec9cfe80fdc94fd2cc81daaec36428d7c409
Viktor <work.morpi@gmail.com>
2019-01-12 19:07:15 +0200

    edit

--- projectCharan/php/handler.php
+++ projectCharan/php/handler.php
@@ -3,18 +3,20 @@
   if (isset($_REQUEST['email']))  {
   
   //Email information
-  $admin_email = "info@cleniro.com";
+  $admin_email = "info@mumbaicurryhouse.com.au";
   $email = $_REQUEST['email'];
   $phone = $_REQUEST['phone'];
   $name = $_REQUEST['name'];
   $text = $_REQUEST['text'];
   
   //send email
-  mail($admin_email, "Waiting list", "The name: $name, phone number: $phone, email: $email and cover letter: $text",  "From:" . $email);
+  mail($admin_email,
+    "Waiting list",
+    "The name: $name,\r\nphone number: $phone,\r\nemail: $email,\r\ncover letter: $text", 
+    "From: $email\r\nReply-To: $email\r\nReturn-Path: $email\r\nContent-Type: text/plain; charset=utf-8\r\nContent-Transfer-Encoding: 8bit");
   
-  header('Location: http://projectCharan/');
-  // echo '<h1 style="text-align: center; margin-top: 10%;">Thank you! We will contact you soon.</h1>'; 
+  // Redirecting, to the main page, after sending a message.
+  header('Location: http://mumbaicurryhouse.com.au/');
   }
-  
   //if "email" variable is not filled out, display the form
 ?>
