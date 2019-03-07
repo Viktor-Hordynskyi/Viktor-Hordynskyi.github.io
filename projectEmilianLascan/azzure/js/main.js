43ba4d99aad07c3ec0e8a74c7de2720feabbee69
Viktor <work.morpi@gmail.com>
2019-03-07 10:45:57 +0200

    edit

--- projectEmilianLascan/azzure/js/main.js
+++ projectEmilianLascan/azzure/js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 		var anchor = $(this);
 		$('html, body').stop().animate({
 			scrollTop: $(anchor.attr('href')).offset().top
-		}, 500, 'linear');
+		}, 1500, 'linear');
 	});
 
 	$('.navbar-toggler').on('click', function() {
