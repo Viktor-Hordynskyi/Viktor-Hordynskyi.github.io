99d84b6059ec92821d6d42781d9a4511fc13b207
Viktor <work.morpi@gmail.com>
2019-03-14 20:02:46 +0200

    edit

---
+++ projectYoKo/inbox/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	// $('.chat__messages').on('click', function() {
+	// 	if($('.chat__message--text.collapse').hasClass('show')) {
+	// 		$('.chat__message').removeClass('opacity')
+	// 	} else {
+	// 		$('.chat__message').addClass('opacity')
+	// 	}
+	// })
+	
+})
