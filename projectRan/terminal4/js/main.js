b0c560f9d3d4456ee011dce51cf5f829bf4b3aad
Viktor <work.morpi@gmail.com>
2019-03-29 12:32:35 +0200

    edit

--- projectRan/terminal4/js/main.js
+++ projectRan/terminal4/js/main.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
 	const footer = $('.footer').height();
-	const header = $('.navbar').innerHeight();
+	const header = $('.navbar').innerHeight();	
 
 	$('.gallery').css('min-height', `calc(100vh - 1rem - ${footer}px - ${header}px)`);
 
+	$('#back-to-top').on('click', function(){
+		$('html, body').stop().animate({scrollTop:0}, 500, "linear");
+	})
 });
 
 $(window).on('resize', function() {
@@ -12,3 +15,4 @@ $(window).on('resize', function() {
 
 	$('.gallery').css('min-height', `calc(100vh - 1rem - ${footer}px - ${header}px)`);
 })
+
