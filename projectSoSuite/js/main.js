d0467a69ce652ec79908c6d839646ae042c9986f
Viktor <work.morpi@gmail.com>
2019-04-22 18:42:10 +0300

    edit

--- projectSoSuite/js/main.js
+++ projectSoSuite/js/main.js
@@ -1,31 +1,41 @@
 $(document).ready(function() {
+	// checking, is this a touch device?
 	let hover = window.matchMedia("(hover)");
 	if(!hover.matches) {
 		$('.touch').addClass('no-touch');
 	}
 
+	// Open/close header menu (on tablet and mobile)
 	$('.header__toggler').on('click', function() {
 		$(this).toggleClass('close');
 		$('.header__navbar').toggle('show')
 	})
 
-	let scroll = $(window).scrollTop();
-	
+	// add a fixed property to header (on load)
+	let scroll = $(window).scrollTop();	
 	if(scroll > 0) {
 		$('.header').addClass('fixed');
 	}
 
+	// add a fixed property to header (on scroll and resize)
 	$(document).on('scroll resize', function() {
 		let scroll = $(window).scrollTop();
+		let header = $('.header').height();
 
-		if(scroll > 0) {
+		// $('.header').addClass('fixed');
+		
+		if(scroll > header/2) {
 			$('.header').addClass('fixed');
 		} else {
 			$('.header').removeClass('fixed');
 		}
 	})	
 
+	// scrolling to anchor
 	let hheader = $('.header').height();
+	$(window).on('resize', function() {
+		let hheader = $('.header').height();
+	})
 	$('a[href^="#"]').on('click', function(e){
 		e.preventDefault();
 		let anchor = $(this);
@@ -36,6 +46,20 @@ $(document).ready(function() {
 		}, speed, 'linear');
 	});
 
+	// $(document).on('resize', function() {
+	// 	let hheader = $('.header').height();
+	// 	$('a[href^="#"]').on('click', function(e){
+	// 		e.preventDefault();
+	// 		let anchor = $(this);
+	// 		let speed = (($(anchor.attr('href')).offset().top) / 8);
+
+	// 		$('html, body').stop().animate({
+	// 			scrollTop: $(anchor.attr('href')).offset().top - hheader
+	// 		}, speed, 'linear');
+	// 	});
+	// })
+
+	// scrolling to anchor (#contact) on home page from other pages
 	if(window.location.hash == '#contact') {
 		location.hash = '';
 		let hheader = $('.header').height();
@@ -44,21 +68,9 @@ $(document).ready(function() {
 		$('html, body').stop().animate({
 			scrollTop: $(anchor).offset().top - hheader
 		}, speed, 'linear');
-	}
+	}	
 
-	$(document).on('resize', function() {
-		let hheader = $('.header').height();
-		$('a[href^="#"]').on('click', function(e){
-			e.preventDefault();
-			let anchor = $(this);
-			let speed = (($(anchor.attr('href')).offset().top) / 8);
-
-			$('html, body').stop().animate({
-				scrollTop: $(anchor.attr('href')).offset().top - hheader
-			}, speed, 'linear');
-		});
-	})
-
+	// Scroll to top
 	$('#back-to-top').on('click', function(){
 		$('html, body').stop().animate({scrollTop:0}, 300, "linear");
 	})
@@ -67,6 +79,7 @@ $(document).ready(function() {
 	let height = $('.faq-page__element-answer.open').prop('scrollHeight');
 	$('.faq-page__element-answer.open').css('max-height', height);
 
+	// scripts on a FAQ page for the accordion
 	$('.accordion').on('click', function() {
 		let css = $(this).next().css('max-height');
 		let height = $(this).next().prop('scrollHeight');
