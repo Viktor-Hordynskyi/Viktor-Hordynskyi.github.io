810617be1f25b317dac0cac60baaee9c1c61e711
Viktor <work.morpi@gmail.com>
2019-05-07 10:39:00 +0300

    edit

--- projectColinHarbut/js/main.js
+++ projectColinHarbut/js/main.js
@@ -11,8 +11,25 @@ $(document).ready(function() {
 	});
 
 	// Bootstrap Lightbox
-	$(document).on('click', '[data-toggle="lightbox"]', function(event) {
-		event.preventDefault();
-		$(this).ekkoLightbox();
+	$(window).on('resize load', function() {
+		if (!window.matchMedia("(max-width: 767.98px)").matches) {
+			$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+				event.preventDefault();
+				$(this).ekkoLightbox({
+					alwaysShowClose: true,
+				});
+			});
+		}
+	})
+
+	// scrolling to anchor
+	$('a[href^="#"]').on('click', function(e){
+		e.preventDefault();
+		let anchor = $(this);
+		let speed = (($(anchor.attr('href')).offset().top) / 4);
+
+		$('html, body').stop().animate({
+			scrollTop: $(anchor.attr('href')).offset().top
+		}, speed, 'linear');
 	});
 })
