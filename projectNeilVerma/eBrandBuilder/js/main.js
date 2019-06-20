812d17e512d9a01090de88f4a3a5b32c7d2401d3
Viktor <work.morpi@gmail.com>
2019-06-20 20:25:49 +0300

    edit

--- projectNeilVerma/eBrandBuilder/js/main.js
+++ projectNeilVerma/eBrandBuilder/js/main.js
@@ -12,6 +12,49 @@ $(document).ready(function() {
 })
 
 // JS for home page
+$(window).on('load', function() {
+	if($('body').prop('id') == 'home-page') {
+		var mySwiper = new Swiper ('.partners__slider', {
+			direction: 'horizontal',
+			loop: true,
+			speed: 750,
+			autoplay: {
+				delay: 5000,
+			},
+			slidesPerView: 4,
+			spaceBetween: 20,
+			breakpoints: {
+				375: {
+					slidesPerView: 1,
+					spaceBetween: 0
+				},
+				576: {
+					slidesPerView: 2,
+					spaceBetween: 10
+				},
+				767: {
+					slidesPerView: 3,
+					spaceBetween: 20
+				}
+			}
+		});
+
+		var mySwiper = new Swiper ('.what__slider', {
+			direction: 'horizontal',
+			loop: true,
+			speed: 400,
+			autoplay: {
+				delay: 5000,
+			},
+			slidesPerView: 1,
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev',
+			},
+		});
+	}
+})
+
 $('#home-page .parallax').on('load scroll', function() {
 	// show navbar when scroll page
 	let w_scroll = $(window).scrollTop();
